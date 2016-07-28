5908ddd9fb8781bc19398ebd8aba17cadfd51589
Sven Fischer <sven.fischer85@gmx.de>
2016-07-28 22:27:19 +0200

    add init.py to initialize DB and .icfpc dir

---
+++ meta/init.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+from sys import argv
+import sqlite3
+import pathlib
+
+def help():
+    "Initialize icfpc in your current directory"
+    "Creates ./.icfp/ directory with database."
+
+def create_dir():
+    p = pathlib.Path("./.icfpc/")
+    if not p.exists():
+        p.mkdir()
+
+def create_db():
+    conn = sqlite3.connect("./.icfpc/db.db")
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
+    cursor.execute("CREATE table versions ("
+                        "id INTEGER PRIMARY KEY,"
+                        "path TEXT UNIQUE ON CONFLICT IGNORE)")
+    cursor.execute("CREATE table tasks ("
+                        "id INTEGER PRIMARY KEY,"
+                        "path TEXT UNIQUE ON CONFLICT IGNORE)")
+    cursor.execute("CREATE table runs("
+                        "id INTEGER PRIMARY KEY,"
+                        "version INTEGER,"
+                        "task INTEGER,"
+                        "path TEXT,"
+                        "score REAL,"
+                        "FOREIGN KEY(version) REFERENCES versions(id),"
+                        "FOREIGN KEY(task) REFERENCES tasks(id))")
+    conn.commit()
+    conn.close()
+
+def main():
+    create_dir()
+    create_db()
+
+if __name__ == "__main__":
+    main()
+
