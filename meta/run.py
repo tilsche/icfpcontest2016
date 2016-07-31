1dca9d43a9696de026e437e11fd5eb07769b1400
Sven Fischer <sven.fischer85@gmx.de>
2016-07-31 04:41:07 +0200

    limit run.py to start only num_cores processes in parallel

--- meta/run.py
+++ meta/run.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 from sys import argv
+import sys
+import os
 import subprocess
 import sqlite3
 import time
 import tempfile
 import pathlib
+import itertools
 
 def help():
     print("Executes multiple different combinations of versions and tasks in parallel.")
@@ -37,23 +40,59 @@ def save_run(version_id, task_id, solutionPath, score):
     conn.commit()
     conn.close()
 
+#http://code.activestate.com/recipes/577376-simple-way-to-execute-multiple-process-in-parallel/
+def cpu_count():
+    ''' Returns the number of CPUs in the system
+    '''
+    num = 1
+    if sys.platform == 'win32':
+        try:
+            num = int(os.environ['NUMBER_OF_PROCESSORS'])
+        except (ValueError, KeyError):
+            pass
+    elif sys.platform == 'darwin':
+        try:
+            num = int(os.popen('sysctl -n hw.ncpu').read())
+        except ValueError:
+            pass
+    else:
+        try:
+            num = os.sysconf('SC_NPROCESSORS_ONLN')
+        except (ValueError, OSError, AttributeError):
+            pass
+
+    return num
+
 def quick(versionPath):
+    def done(c):
+        return c[0].poll() is not None
+
     children = []
     tasks = get_tasks()
-    for t in tasks:
-       children.append(subprocess.Popen(["./execute.py", versionPath, t[1]], universal_newlines=True, stdout=subprocess.PIPE))
-    print("done with creating threads")
-    #sequentially waiting, bad for already using early results 
-    for c,t in zip(children, tasks):
-        out = c.communicate()[0]
-        solution = tempfile.NamedTemporaryFile(dir="./.icfpc/solutions", delete=False)
-        solution.write(bytes(out, "utf-8"))
-        solution.close()
-        score = subprocess.check_output(["./evaluate.py", t[1], solution.name], universal_newlines=True)
-        print("task:\t" + t[1])
-        print("score:\t" + score)
-        solutionPath = pathlib.Path(solution.name).relative_to(pathlib.Path().resolve())
-        save_run(get_version(versionPath)[0], t[0], str(solutionPath), score)
+    max_tasks = cpu_count()
+    print("Found " + str(max_tasks) + " cores", file=sys.stderr)
+    while True:
+        while tasks and len(children) < max_tasks:
+            task = tasks.pop()
+            children.append((subprocess.Popen(["./execute.py", versionPath, task[1]], universal_newlines=True, stdout=subprocess.PIPE), task))
+            print("Created a thread", file=sys.stderr)
+
+        for ct in filter(done, children):
+            children.remove(ct)
+            c, t = ct
+            with tempfile.NamedTemporaryFile(dir="./.icfpc/solutions", delete=False) as solution:
+                solution.write(bytes(c.communicate()[0], "utf-8"))
+
+            score = subprocess.check_output(["./evaluate.py", t[1], solution.name], universal_newlines=True)
+            print("task:\t" + t[1])
+            print("score:\t" + score)
+            solutionPath = pathlib.Path(solution.name).relative_to(pathlib.Path().resolve())
+            save_run(get_version(versionPath)[0], t[0], str(solutionPath), score)
+
+        if not children and not tasks:
+            break
+        else:
+            time.sleep(0.05)#dunno if that's reasonable
 
 def main():
     if len(argv) < 3:
