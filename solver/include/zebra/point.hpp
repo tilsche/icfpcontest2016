7481bf81039e5468d3854ca4eb8e604e6bcf11dd
Ronny <ronnybrendel@gmail.com>
2016-08-05 12:35:47 +0200

    fix

--- solver/include/zebra/point.hpp
+++ solver/include/zebra/point.hpp
@@ -15,16 +15,16 @@ class point {
 
         static point from_string(const std::string& s) {
 
-            int first_slash = -1, comma = -1, second_slash = -1;
+            int comma = -1;
             for (int i = 0; i < s.size(); i += 1) {
-                if (first_slash != -1 && comma == -1 && s[i] == ',') {
+                if (comma == -1 && s[i] == ',') {
                     comma = i;
                 }
 
                 assert(s[i] == ',' || s[i] == '/' || (s[i] >= '0' && s[i] <= '9'));
             }
 
-            assert(comma        != -1);
+            assert(comma != -1);
 
             std::string x = s.substr(0, comma);
             std::string y = s.substr(comma+1, s.size() - (comma+1));
