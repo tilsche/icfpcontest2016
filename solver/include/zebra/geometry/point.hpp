54ac7e323fe6d09c7eaa63f2b6fee570584c7fe3
Ronny <ronnybrendel@gmail.com>
2016-08-05 15:12:29 +0200

    ff

--- solver/include/zebra/geometry/point.hpp
+++ solver/include/zebra/geometry/point.hpp
@@ -7,14 +7,17 @@
 
 #include <cassert>
 #include <iostream>
+#include <sstream>
 #include <string>
 
 namespace zebra
 {
 
 using point = kernel::Point_2;
-static point point_from_string(std::string s)
+
+point point_from_string(const std::string& s_)
 {
+    std::string s(s_);
     for (int i = 0; i < s.size(); i += 1)
     {
         if (s[i] == ',')
@@ -28,82 +31,27 @@ static point point_from_string(std::string s)
     ss >> y;
     return point(x, y);
 }
+
+std::string gmpq_to_string(const CGAL::Gmpq& q) {
+    std::stringstream ss;
+    ss << q;
+    std::string s = ss.str();
+
+    if (s.size() < 3) { return s; }
+
+    if (s[s.size()-2] == '/' && s[s.size()-1] == '1') {
+        return s.substr(0, s.size()-2);
+    } else {
+        return s;
+    }
 }
-/*
-public:
-    mpq_class x, y;
 
-    point() : x(-1), y(-1)
-    {
-    }
-    point(const point& p) : x(p.x), y(p.y)
-    {
-    }
-    point(const mpq_class& x_, const mpq_class& y_) : x(x_), y(y_)
-    {
-    }
-
-    point& operator=(const point& rhs)
-    {
-        x = rhs.x;
-        y = rhs.y;
-        return *this;
-    }
-
-    point operator+(const point& rhs) const
-    {
-        return point(x + rhs.x, y + rhs.y);
-    }
-    point operator-(const point& rhs) const
-    {
-        return point(x - rhs.x, y - rhs.y);
-    }
-    point operator*(const mpq_class f) const
-    {
-        return point(x * f, y * f);
-    }
-    bool operator==(const point& rhs) const
-    {
-        return x == rhs.x && y == rhs.y;
-    }
-    bool operator!=(const point& rhs) const
-    {
-        return !(*this == rhs);
-    }
-
-    static point from_string(const std::string& s)
-    {
-
-        int comma = -1;
-        for (int i = 0; i < s.size(); i += 1)
-        {
-            if (comma == -1 && s[i] == ',')
-            {
-                comma = i;
-            }
-
-            assert(s[i] == ',' || s[i] == '/' || s[i] == '-' || (s[i] >= '0' && s[i] <= '9'));
-        }
-
-        assert(comma != -1);
-
-        std::string x = s.substr(0, comma);
-        std::string y = s.substr(comma + 1, s.size() - (comma + 1));
-
-        point p;
-        mpq_set_str(p.x.get_mpq_t(), x.c_str(), 10);
-        mpq_set_str(p.y.get_mpq_t(), y.c_str(), 10);
-
-        return p;
-    }
-};
-
-std::ostream& operator<<(std::ostream& o, const point& p)
-{
-    o << p.x << "," << p.y;
-    return o;
+std::string point_to_string(const point& p) {
+    std::stringstream s;
+    s << gmpq_to_string(p.x()) << "," << gmpq_to_string(p.y());
+    return s.str();
 }
 
 };
-*/
+
 #endif // ZEBRA_POINT_HPP
