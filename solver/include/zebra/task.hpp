15e4c3b684b4a9bb3fd3db6577902164df083931
Sebastian Oeste <sebastian.oeste@tu-dresden.de>
2016-08-05 12:52:42 +0200

    fix typos

--- solver/include/zebra/task.hpp
+++ solver/include/zebra/task.hpp
@@ -2,11 +2,11 @@
 #define __ZEBRA_TASK_HPP__
 
 #include <iostream>
-#include <ssteam>
+#include <fstream>
 #include <functional>
 #include <vector>
 
-#include <boost/algorithm/sting.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/filesystem.hpp>
 
 #include "point.hpp"
@@ -16,7 +16,10 @@ namespace zebra {
 
 bool for_each_line(const std::string& filename, std::function<bool(const std::string&)> cb)
 {
-    std::ifstream in{filename};
+    std::ifstream in {filename};
+    if (!in) {
+        return false;
+    }
 
     for (std::string line; getline(in, line);) {
         if (! cb(line)) {
@@ -33,6 +36,7 @@ std::vector<point> read_task(const std::string& taskfile)
 
     for_each_line(taskfile, [&](const std::string& line) {
             data.emplace_back(point::from_string(line));
+            return true;
         });
 
     return data;
