f18bf2ef7ab4c376cd5ee7962d56cc0338e1125f
Ronny <ronnybrendel@gmail.com>
2016-08-05 12:54:14 +0200

    a few operators and tests

--- solver/tests/point_test.cpp
+++ solver/tests/point_test.cpp
@@ -1,6 +1,9 @@
 #include <zebra/point.hpp>
 using namespace zebra;
 
+#include <sstream>
+using namespace std;
+
 void test1();
 
 int main() {
@@ -8,7 +11,19 @@ int main() {
 }
 
 void test1() {
-
     point p = point::from_string("1/2,3/4");
+    assert(p.x.get_num() == 1);
+    assert(p.x.get_den() == 2);
+    assert(p.y.get_num() == 3);
+    assert(p.y.get_den() == 4);
 
+    stringstream s;
+    s << p;
+    assert(s.str() == "1/2,3/4");
+
+    point q(p);
+    point r = p;
+
+    assert(q == p);
+    assert(r == p);
 }
