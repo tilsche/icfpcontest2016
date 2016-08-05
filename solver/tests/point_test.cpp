1ab9aa62e8ba54b8d0259480b38d37509b7b4ab7
Ronny <ronnybrendel@gmail.com>
2016-08-05 13:51:43 +0200

    new point test

--- solver/tests/point_test.cpp
+++ solver/tests/point_test.cpp
@@ -17,13 +17,23 @@ void test1() {
     assert(p.y.get_num() == 3);
     assert(p.y.get_den() == 4);
 
-    stringstream s;
-    s << p;
-    assert(s.str() == "1/2,3/4");
+    stringstream s1;
+    s1 << p;
+    assert(s1.str() == "1/2,3/4");
 
     point q(p);
     point r = p;
 
     assert(q == p);
     assert(r == p);
+
+    p = point::from_string("1,2");
+    assert(p.x.get_num() == 1);
+    assert(p.x.get_den() == 1);
+    assert(p.y.get_num() == 2);
+    assert(p.y.get_den() == 1);
+
+    stringstream s2;
+    s2 << p;
+    assert(s2.str() == "1,2");
 }
