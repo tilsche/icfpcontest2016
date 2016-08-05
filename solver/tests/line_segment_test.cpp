98286a584328d18c75163216973880897c4993c1
Ronny <ronnybrendel@gmail.com>
2016-08-05 15:46:37 +0200

    f

--- solver/tests/line_segment_test.cpp
+++ solver/tests/line_segment_test.cpp
@@ -5,28 +5,26 @@ using namespace zebra;
 using namespace std;
 
 void test1();
-void test_slope();
 void test_disjoint();
 
 int main() {
     test1();
-    test_slope();
     test_disjoint();
 }
 
 void test1() {
-    line_segment l = line_segment::from_string("1/2,3/4 5/6,7/8");
-    assert(l.a.x.get_num() == 1);
-    assert(l.a.x.get_den() == 2);
-    assert(l.a.y.get_num() == 3);
-    assert(l.a.y.get_den() == 4);
-    assert(l.b.x.get_num() == 5);
-    assert(l.b.x.get_den() == 6);
-    assert(l.b.y.get_num() == 7);
-    assert(l.b.y.get_den() == 8);
+    line_segment l = line_segment_from_string("1/2,3/4 5/6,7/8");
+    assert(l.source().x().numerator()   == 1);
+    assert(l.source().x().denominator() == 2);
+    assert(l.source().y().numerator()   == 3);
+    assert(l.source().y().denominator() == 4);
+    assert(l.target().x().numerator()   == 5);
+    assert(l.target().x().denominator() == 6);
+    assert(l.target().y().numerator()   == 7);
+    assert(l.target().y().denominator() == 8);
 
     stringstream s;
-    s << l;
+    s << line_segment_to_string(l);
     assert(s.str() == "1/2,3/4 5/6,7/8");
 
     line_segment m(l);
@@ -36,27 +34,16 @@ void test1() {
     assert(n == l);
 }
 
-void test_slope() {
-    line_segment l1 = line_segment::from_string("1,1 2,2");
-    line_segment l2 = line_segment::from_string("2,2 1,1");
-    line_segment l3 = line_segment::from_string("0,0 0,1");
-    line_segment l4 = line_segment::from_string("0,0 0,-1");
-
-    assert(l1.slope() == 1);
-    assert(l2.slope() == 1);
-    assert(l3.slope_inf() == true);
-    assert(l4.slope_inf() == true);
-}
-
 void test_disjoint() {
-    line_segment l1 = line_segment::from_string("1,1 2,2");
-    line_segment l2 = line_segment::from_string("2,2 3,3");
-    line_segment l3 = line_segment::from_string("3,3 4,4");
-    line_segment l4 = line_segment::from_string("0,0 0,1");
+    line_segment l1 = line_segment_from_string("1,1 2,2");
+    line_segment l2 = line_segment_from_string("2,2 3,3");
+    line_segment l3 = line_segment_from_string("3,3 4,4");
+    line_segment l4 = line_segment_from_string("0,0 0,1");
 
-    line_segment l5 = line_segment::from_string("3/2,3/2 7/4,7/4");
-    line_segment l6 = line_segment::from_string("3/2,3/2 3,3");
+    line_segment l5 = line_segment_from_string("3/2,3/2 7/4,7/4");
+    line_segment l6 = line_segment_from_string("3/2,3/2 3,3");
 
+    //std::cerr << l1 << "," << l2 << std::endl;
     assert(is_disjoint(l1, l2));
     assert(is_disjoint(l2, l3));
     assert(is_disjoint(l1, l3));
