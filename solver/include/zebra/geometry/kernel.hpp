ad4fb6c002cb25f0c97aef8f99f4210b7624bfbb
tilsche <thomas.ilsche@tu-dresden.de>
2016-08-05 19:14:58 +0200

    minor kernel stuff

--- solver/include/zebra/geometry/kernel.hpp
+++ solver/include/zebra/geometry/kernel.hpp
@@ -1,12 +1,17 @@
 #ifndef SOLVER_GEOMETRY_KERNEL
 #define SOLVER_GEOMETRY_KERNEL
 
-#include <CGAL/Simple_cartesian.h>
 #include <CGAL/Gmpz.h>
+#include <CGAL/Simple_cartesian.h>
 
 namespace zebra
 {
 using kernel = CGAL::Simple_cartesian<CGAL::Gmpq>;
+
+inline double gmpq_to_double(CGAL::Gmpq q)
+{
+    return q.numerator().to_double() / q.denominator().to_double();
+}
 }
 
 #endif
