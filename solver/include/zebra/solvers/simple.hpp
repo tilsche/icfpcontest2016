a14eff85dadfaf7e2d323e1154e10e6c3ff40076
tilsche <thomas.ilsche@tu-dresden.de>
2016-08-05 19:47:03 +0200

    towards origami

--- solver/include/zebra/solvers/simple.hpp
+++ solver/include/zebra/solvers/simple.hpp
@@ -14,16 +14,34 @@ public:
     solution operator()(task t) override
     {
         solution s;
-        polygon one;
-        one.push_back({ 0, 0 });
-        one.push_back({ 1, 0 });
-        one.push_back({ 1, 1 });
-        one.push_back({ 0, 1 });
-        s.source_positions = one;
-        s.destination_positions = one;
+        s.source_positions = make_polygon_1();
+        s.destination_positions = make_polygon_1();
         s.facets.push_back({ 0, 1, 2, 3 });
+
+        assert(t.sil.polygons.size() == 1);
+        polygon target = t.sil.polygons[0];
+        assert(target.is_counterclockwise_oriented());
+
+        origami ori;
+        double best_r = 0;
+        origami best_ori;
+        for (auto vx_it = target.vertices_begin(); vx_it != target.vertices_end(); vx_it++)
+        {
+            for (auto o2 : ori.move_to(*vx_it))
+            {
+                auto new_r = o2.resemblance(target);
+                if (new_r > best_r)
+                {
+                    best_r = new_r;
+                    best_ori = o2;
+                }
+            }
+        }
+        logging::info() << "Simple found best resemblence " << best_r;
+
+        //        s.destination_positions
         return s;
     }
 };
 }
-#endif //SOLVER_SIMPLE_HPP
+#endif // SOLVER_SIMPLE_HPP
