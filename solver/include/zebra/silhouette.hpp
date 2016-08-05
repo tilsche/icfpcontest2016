58bcd6e31a729df8bec8d48dce6b31bef0b5276d
tilsche <thomas.ilsche@tu-dresden.de>
2016-08-05 12:53:19 +0200

    trinagle

--- solver/include/zebra/silhouette.hpp
+++ solver/include/zebra/silhouette.hpp
@@ -2,4 +2,18 @@
 #ifndef SOLVER_SILHOUETTE_HPP
 #define SOLVER_SILHOUETTE_HPP
 
-#endif //SOLVER_SILHOUETTE_HPP
+#include <vector>
+
+namespace zebra
+{
+class silhouette
+{
+public:
+    silhouette(std::vector<polygon> p) : polygons(p)
+    {
+    }
+    std::vector<polygon> polygons;
+};
+}
+
+#endif // SOLVER_SILHOUETTE_HPP
--- solver/include/zebra/triangle.hpp
+++ solver/include/zebra/triangle.hpp
@@ -2,4 +2,16 @@
 #ifndef SOLVER_TRIANGLE_HPP
 #define SOLVER_TRIANGLE_HPP
 
-#endif //SOLVER_TRIANGLE_HPP
+#endif // SOLVER_TRIANGLE_HPP
+
+#include "point.hpp"
+
+#include <array>
+
+namespace zebra
+{
+class triangle
+{
+    std::array<point, 3> points;
+};
+}
