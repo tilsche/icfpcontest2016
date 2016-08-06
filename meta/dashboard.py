3eb6cda8b71cf2d1c51c825b2e308a382b5a9bc8
Sven Fischer <sven.fischer85@gmx.de>
2016-08-06 13:07:44 +0200

    add meta rest api to dashboard

--- meta/dashboard.py
+++ meta/dashboard.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 from flask import Flask
+from models import *
 import report
+import playhouse.shortcuts
 
 app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
     return "<pre>" + report.table() + "</pre>"
+
+@app.route("/best/")
+def best():
+    r = Run.select(Run, Task.path, fn.MAX(Run.score).alias("max")).join(Task).group_by(Run.task).get()
+    return str(playhouse.shortcuts.model_to_dict(r))
 
 if __name__ == "__main__":
     app.run()
