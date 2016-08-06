e4b333a0a5afced154c6ad3c0a2f9c4a514ba599
Ronny <ronnybrendel@gmail.com>
2016-08-06 11:44:44 +0200

    f

--- solver/include/zebra/solution.hpp
+++ solver/include/zebra/solution.hpp
@@ -4,6 +4,8 @@
 
 #include "geometry.hpp"
 
+#include <zebra/log.hpp>
+
 #include <cassert>
 #include <fstream>
 #include <iostream>
@@ -109,7 +111,51 @@ struct solution
 
     void fold(const line_segment& fold) {
 
-        // TODO test that fold is a correct line .. has_on()
+        auto verify_fold = [this](const line_segment& fold) {
+            int intersections = 0;
+            int is_ons        = 0;
+            std::vector<line_segment> segments;
+            for (auto facet : facets) {
+                for (int i = 1; i < facet.size(); i += 1) {
+                    segments.push_back(line_segment(destination_positions[i-1], destination_positions[i]));
+                }
+                segments.push_back(line_segment(destination_positions[facet.size()-1], destination_positions[0]));
+            }
+
+            for (const auto& s : segments) {
+                boost::optional<boost::variant<point, line_segment>> o = CGAL::intersection(s, fold);
+
+                if (o == boost::none) { continue; }
+
+                if (o->which() == 0) {
+                    intersections += 1;
+                } else {
+                    logging::error() << "folding on a line segment, fold:" << line_segment_to_string(fold) << ", segment:" << line_segment_to_string(s);
+                }
+
+                assert(! (s.has_on(fold.source()) && s.has_on(fold.target())));
+
+                if (s.has_on(fold.source()) || s.has_on(fold.target())) {
+                    is_ons += 1;
+                }
+            }
+
+            if (is_ons == 0) {
+                logging::error() << "Fold does not start and end on any line segment";
+                assert(is_ons > 0);
+            }
+
+            if (intersections % 2 != 0) {
+                logging::warn() << "Fold intersects with an odd number of segments: " << intersections;
+            }
+
+            if (is_ons % 2 != 0) {
+                logging::warn() << "Fold ends/starts on an odd number of segments: " << is_ons;
+            }
+        };
+
+        verify_fold(fold);
+
 
         // intersection points
 
