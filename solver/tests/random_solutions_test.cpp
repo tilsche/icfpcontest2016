681ce69799105e735fc389d8e581c8d9ce864ba8
Ronny <ronnybrendel@gmail.com>
2016-08-06 17:20:22 +0200

    f

--- solver/tests/random_solutions_test.cpp
+++ solver/tests/random_solutions_test.cpp
@@ -6,5 +6,19 @@ int main(int argc, char** args) {
 
     srand(time(NULL));
 
+    auto s = solution::unit_square();
+
+    s.to_png("unit_square");
+
+    polygon hull = s.hull();
+
+    for (auto i = hull.edges_begin(); i != hull.edges_end(); ++i) {
+        cerr << point_to_string(i->source()) << " " << point_to_string(i->target()) << endl;
+    }
+
+    int edge_count = hull.size();
+
+    std::cerr << "edge_count" << edge_count;
+
     return 0;
 }
