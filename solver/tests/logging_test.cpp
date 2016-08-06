935b0c13876d5f1a81d27d8bcc91a8b8a13d64e4
Mario Bielert <mario.bielert@googlemail.com>
2016-08-06 11:21:20 +0200

    Don’t mess with Voodoo

--- solver/tests/logging_test.cpp
+++ solver/tests/logging_test.cpp
@@ -1,7 +1,14 @@
 #include <zebra/log.hpp>
 
+#include <vector>
+
 int main()
 {
+    std::vector v;
+
     zebra::logging::info() << "Hello, World!";
+
+    zebra::logging::info() << v;
+
     return 0;
 }
