1a7432aaa3972b46d3f01bb387997cdc43987699
Ronny <ronnybrendel@gmail.com>
2016-08-06 10:23:40 +0200

    f

---
+++ solver/tests/solution_fold_test.cpp
@@ -0,0 +1,49 @@
+#include <zebra/solution.hpp>
+using namespace std;
+using namespace zebra;
+
+void test1();
+void test2();
+
+int main() {
+    test1();
+    test2();
+
+    return 0;
+}
+
+void test1() {
+
+    solution s;
+    s.source_positions      = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+    s.facets                = {{0, 1, 2, 3}};
+    s.destination_positions = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+
+    line_segment fold = line_segment_from_string("0,1/2 1,3/4");
+
+    auto o = solution_fold(s, fold);
+    // TODO
+}
+
+void test2() {
+
+    // +---------+
+    // |         |
+    // +---------+
+    // |         |
+    // +---------+
+
+    solution s;
+    s.source_positions = {{0, 0}, {1, 0}, {1, 1}, {0, 1},
+                          {0, 1/2}, {1, 1/2}};
+
+    s.facets = { {0, 1, 4, 5}, {4, 5, 2, 3}};
+
+    s.destination_positions = {{0, 0}, {1, 0}, {1, 0}, {0, 0},
+                               {0, 1/2}, {1, 1/2}};
+
+    line_segment fold = line_segment_from_string("1/2,0 1/2,1");
+
+    auto o = solution_fold(s, fold);
+    // TODO
+}
