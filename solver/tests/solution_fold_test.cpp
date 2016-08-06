44db08ad2deb36d1d7088e62421e9030a48a74aa
Ronny <ronnybrendel@gmail.com>
2016-08-06 12:21:43 +0200

    f

--- solver/tests/solution_fold_test.cpp
+++ solver/tests/solution_fold_test.cpp
@@ -5,11 +5,13 @@ using namespace zebra;
 void test1();
 void test2();
 void test3();
+void test4();
 
 int main() {
     test1();
     test2();
     test3();
+    test4();
 
     return 0;
 }
@@ -57,9 +59,7 @@ void test2() {
     s.facets                = {{0, 1, 2, 3}};
     s.destination_positions = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
 
-    line_segment fold = line_segment_from_string("0,1/2 1,3/4");
-
-    s.fold(fold);
+    s.fold(line_segment_from_string("0,1/2 1,3/4"));
     //
     //
     // +---+
@@ -93,9 +93,7 @@ void test3() {
     s.destination_positions = {{0,   0}, {1,   0}, {1, 0}, {0, 0},
                                {0, 1/2}, {1, 1/2}};
 
-    line_segment fold = line_segment_from_string("1/2,0 1/2,1");
-
-    s.fold(fold);
+    s.fold(line_segment_from_string("1/2,0 1/2,1"));
     //
     //
     //    +--+
@@ -104,4 +102,48 @@ void test3() {
 
     // TODO check
     //assert(s.facets.size() == 4);
+}
+
+void test4() {
+    //cerr << __func__ << endl;
+
+    //   +---------+
+    //   |         |
+    //   +---------+
+    //   |         |
+    //   +---------+
+    //   |         |
+    //   +---------+
+    //   |         |
+    // 0 +---------+
+
+    solution s;
+    s.source_positions = {{0, 0}, {1, 0}, {1, 1}, {0, 1}};
+    s.facets = {{0, 1, 2, 3}};
+    s.destination_positions = {{0,   0}, {1,   0}, {1, 0}, {0, 0}};
+
+    s.fold(line_segment_from_string("0,3/4 1/3/4"));
+    //   +---------+
+    //   |         |
+    //   +---------+
+    //   |         |
+    //   +---------+
+    //   |         |
+    // 0 +---------+
+
+    s.fold(line_segment_from_string("0,1/4 1/1/4"));
+    //   +---------+
+    //   |         |
+    // 0 +---------+
+    //   |         |
+    //   +---------+
+
+    // on 0 there is now a line on which we want to fold
+
+    s.fold(line_segment_from_string("0,0 1,0"));
+    // 0 +---------+
+    //   |         |
+    //   +---------+
+
+    // TODO check
 }
