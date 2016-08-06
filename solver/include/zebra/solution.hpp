19a5135066401ec463437de25303af979a08116d
Ronny <ronnybrendel@gmail.com>
2016-08-06 12:59:12 +0200

    f

--- solver/include/zebra/solution.hpp
+++ solver/include/zebra/solution.hpp
@@ -392,6 +392,113 @@ struct solution
 
         // // TODO deduplicate segments
     }
+
+    void to_png(const std::string& prefix) {
+        //std::vector<point> source_positions;
+        //std::vector<facet> facets;
+        //std::vector<point> destination_positions;
+
+        // source points
+
+        auto source_points_dat = prefix + "_source_points.dat";
+        std::ofstream os(source_points_dat);
+        for (const auto& p : source_positions) {
+            os << gmpq_to_double(p.x()) << ' ' << gmpq_to_double(p.y()) << std::endl;
+        }
+        os.close();
+
+        auto source_points_plot = prefix + "_source_points.plot";
+        std::ofstream oss(source_points_plot);
+        oss << "set terminal pngcairo size 800,800" << std::endl;
+        oss << "set output \"" << prefix << "_source_points.png\"" << std::endl;
+        oss << "plot \"" << source_points_dat << "\" ps 3 pt 3";
+        oss.close();
+
+        system(("gnuplot " + source_points_plot).c_str());
+
+        unlink(source_points_plot.c_str());
+        unlink(source_points_dat.c_str());
+
+        // destination points
+
+        auto destination_points_dat = prefix + "_destination_points.dat";
+        std::ofstream od(destination_points_dat);
+        for (const auto& p : destination_positions) {
+            od << gmpq_to_double(p.x()) << ' ' << gmpq_to_double(p.y()) << std::endl;
+        }
+        od.close();
+
+        auto destination_points_plot = prefix + "_destination_points.plot";
+        std::ofstream odd(destination_points_plot);
+        odd << "set terminal pngcairo size 800,800" << std::endl;
+        odd << "set output \"" << prefix << "_destination_points.png\"" << std::endl;
+        odd << "plot \"" << destination_points_dat << "\" ps 3 pt 3";
+        odd.close();
+
+        system(("gnuplot " + destination_points_plot).c_str());
+
+        unlink(destination_points_plot.c_str());
+        unlink(destination_points_dat.c_str());
+
+        //for (std::size_t i = 0; i < v.size(); i += 1)
+        //{
+        //    o << " \"" << f << "_" << i << ".dat\" using 1:2:($3-$1):($4-$2) title \"Polygon_" << i
+        //      << "\" with vectors nohead lw 3";
+        //for (std::size_t i = 0; i < v.size(); i += 1)
+        //{
+        //    const auto& polygon = v[i];
+
+        //    std::stringstream s;
+        //    s << f << "_" << i << ".dat";
+
+        //    std::ofstream of(s.str());
+
+        //    for (auto v = polygon.edges_begin(); v != polygon.edges_end(); ++v)
+        //    {
+        //        point a = v->source();
+        //        point b = v->target();
+
+        //        of << gmpq_to_double(a.x()) << ' ' << gmpq_to_double(a.y()) << ' '
+        //           << gmpq_to_double(b.x()) << ' ' << gmpq_to_double(b.y()) << std::endl;
+        //    }
+
+        //    of.close();
+        //}
+
+        //std::ofstream o(f + ".plot");
+
+        //o << "set terminal pngcairo size 800,800" << std::endl;
+        //o << "set output \"" << f << ".png\"" << std::endl;
+        //o << "plot ";
+
+        //for (std::size_t i = 0; i < v.size(); i += 1)
+        //{
+        //    o << " \"" << f << "_" << i << ".dat\" using 1:2:($3-$1):($4-$2) title \"Polygon_" << i
+        //      << "\" with vectors nohead lw 3";
+
+        //    if (i < v.size() - 1)
+        //    {
+        //        o << ", \\" << std::endl;
+        //    }
+        //    else
+        //    {
+        //        o << std::endl;
+        //    }
+        //}
+
+        //o.close();
+
+        //system(("gnuplot " + f + ".plot").c_str());
+
+        //unlink((f + ".plot").c_str());
+
+        //for (std::size_t i = 0; i < v.size(); i += 1)
+        //{
+        //    std::stringstream ss;
+        //    ss << f << "_" << i << ".dat";
+        //    unlink(ss.str().c_str());
+        //}
+    }
 };
 
 inline std::ostream& operator<<(std::ostream& os, const solution& s)
