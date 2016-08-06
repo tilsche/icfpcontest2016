3ddc06ae410dbafaf3b139363065319afb37fe26
tilsche <thomas.ilsche@tu-dresden.de>
2016-08-06 09:38:13 +0200

    solution positions are now just vectors

--- solver/include/zebra/geometry/polygon.hpp
+++ solver/include/zebra/geometry/polygon.hpp
@@ -17,17 +17,28 @@ using polygon_with_holes = CGAL::Polygon_with_holes_2<kernel>;
 inline polygon make_polygon_1()
 {
     polygon p;
-    p.push_back({0, 0});
-    p.push_back({1, 0});
-    p.push_back({1, 1});
-    p.push_back({0, 1});
+    p.push_back({ 0, 0 });
+    p.push_back({ 1, 0 });
+    p.push_back({ 1, 1 });
+    p.push_back({ 0, 1 });
     return p;
 }
 
+inline std::vector<point> make_positions_1()
+{
+    std::vector<point> p;
+    p.push_back({ 0, 0 });
+    p.push_back({ 1, 0 });
+    p.push_back({ 1, 1 });
+    p.push_back({ 0, 1 });
+    return p;
+}
 // not tested
-void polygons_to_png(const std::vector<polygon>& v, const std::string f) {
+void polygons_to_png(const std::vector<polygon>& v, const std::string f)
+{
 
-    for (std::size_t i = 0; i < v.size(); i += 1) {
+    for (std::size_t i = 0; i < v.size(); i += 1)
+    {
         const auto& polygon = v[i];
 
         std::stringstream s;
@@ -35,11 +46,13 @@ void polygons_to_png(const std::vector<polygon>& v, const std::string f) {
 
         std::ofstream of(s.str());
 
-        for (auto v = polygon.edges_begin(); v != polygon.edges_end(); ++v) {
+        for (auto v = polygon.edges_begin(); v != polygon.edges_end(); ++v)
+        {
             point a = v->source();
             point b = v->target();
 
-            of << gmpq_to_double(a.x()) << ' ' << gmpq_to_double(a.y()) << ' ' << gmpq_to_double(b.x()) << ' ' << gmpq_to_double(b.y()) << std::endl;
+            of << gmpq_to_double(a.x()) << ' ' << gmpq_to_double(a.y()) << ' '
+               << gmpq_to_double(b.x()) << ' ' << gmpq_to_double(b.y()) << std::endl;
         }
 
         of.close();
@@ -51,12 +64,17 @@ void polygons_to_png(const std::vector<polygon>& v, const std::string f) {
     o << "set output \"" << f << ".png\"" << std::endl;
     o << "plot ";
 
-    for (std::size_t i = 0; i < v.size(); i += 1) {
-        o << " \"" << f << "_" << i << ".dat\" using 1:2:($3-$1):($4-$2) title \"Polygon_" << i << "\" with vectors nohead lw 3";
+    for (std::size_t i = 0; i < v.size(); i += 1)
+    {
+        o << " \"" << f << "_" << i << ".dat\" using 1:2:($3-$1):($4-$2) title \"Polygon_" << i
+          << "\" with vectors nohead lw 3";
 
-        if (i < v.size()-1) {
+        if (i < v.size() - 1)
+        {
             o << ", \\" << std::endl;
-        } else {
+        }
+        else
+        {
             o << std::endl;
         }
     }
@@ -67,13 +85,13 @@ void polygons_to_png(const std::vector<polygon>& v, const std::string f) {
 
     unlink((f + ".plot").c_str());
 
-    for (std::size_t i = 0; i < v.size(); i += 1) {
+    for (std::size_t i = 0; i < v.size(); i += 1)
+    {
         std::stringstream ss;
         ss << f << "_" << i << ".dat";
         unlink(ss.str().c_str());
     }
 }
-
 }
 
 #endif // SOLVER_POLYGON_HPP
