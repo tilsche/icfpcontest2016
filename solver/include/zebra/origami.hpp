b7dad6abc140b69409fb3ebb6b922770003cb910
tilsche <thomas.ilsche@tu-dresden.de>
2016-08-06 18:23:29 +0200

    prepare brute force

--- solver/include/zebra/origami.hpp
+++ solver/include/zebra/origami.hpp
@@ -3,6 +3,7 @@
 #define SOLVER_ORIGAMI_HPP
 
 #include <zebra/geometry.hpp>
+#include <zebra/log.hpp>
 #include <zebra/solution.hpp>
 
 #include <CGAL/Boolean_set_operations_2.h>
@@ -12,42 +13,24 @@ namespace zebra
 class origami
 {
 public:
-    solution sol = make_solution_1();
+    solution sol = solution::unit_square();
 
     std::vector<origami> move_to(point p) const
     {
+        logging::debug() << "MOVE_TO (" << p << ")[[[[\n" << sol << "\n]]]]] MOVE_TO";
         std::vector<origami> results;
         results.reserve(sol.destination_positions.size() * 2);
-        for (auto dp : sol.destination_positions) {
+        for (auto dp : sol.destination_positions)
         {
             transformation move(CGAL::TRANSLATION, p - dp);
             auto moved = *this;
             moved.sol.transform(move);
+            logging::trace() << "MOVED (" << dp << ")[[[[\n" << moved.sol << "\n]]]]] MOVED";
+
             results.push_back(moved);
         }
         return results;
     }
-
-    double resemblance(polygon target) const
-    {
-        polygon_with_holes p_or;
-        std::vector<polygon_with_holes> p_ands;
-        CGAL::join(poly, target, p_or);
-        CGAL::intersection(poly, target, std::back_inserter(p_ands));
-        CGAL::Gmpq and_area = 0;
-        for (const auto& holy : p_ands)
-        {
-            and_area += holy.outer_boundary().area();
-        }
-        auto or_area = p_or.outer_boundary().area();
-        return gmpq_to_double(and_area / or_area);
-    }
-
-    size_t size() const
-    {
-        return poly.size();
-    }
-
 };
 }
 
