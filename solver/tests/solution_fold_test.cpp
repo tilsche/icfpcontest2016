60a01ece23d5ac6a3d04277bbde7439821fd9d06
Ronny <ronnybrendel@gmail.com>
2016-08-06 11:44:44 +0200

    f

--- solver/tests/solution_fold_test.cpp
+++ solver/tests/solution_fold_test.cpp
@@ -21,7 +21,7 @@ void test1() {
 
     line_segment fold = line_segment_from_string("0,1/2 1,3/4");
 
-    auto o = s.fold(fold);
+    s.fold(fold);
     // TODO
 }
 
@@ -44,6 +44,6 @@ void test2() {
 
     line_segment fold = line_segment_from_string("1/2,0 1/2,1");
 
-    auto o = s.fold(fold);
+    s.fold(fold);
     // TODO
 }
