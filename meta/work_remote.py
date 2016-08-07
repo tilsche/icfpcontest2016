7fbf127fdae2b22d78daeac1200ca74ef0c27e5d
Sven Fischer <sven.fischer85@gmx.de>
2016-08-07 13:52:38 +0200

    multithread meta 1

--- meta/work_remote.py
+++ meta/work_remote.py
@@ -51,36 +51,44 @@ PATH_GIT_REMOTE = ".icfpc/git_remote/"
 #REPO = "https://github.com/na-oma/test.git"
 REPO = "git@github.com:tilsche/icfpcontest2016.git"
 
+def clone_and_build(version):
+    #clone git + build
+    path = PATH_GIT_REMOTE + version.reference + "/"
+    if not pathlib.Path(path).exists():
+        print("cloning into " + path)
+        out = subprocess.check_output(["git", "clone", REPO, path], universal_newlines=True)
+        print(out)
+        print("checkout " + version.reference)
+        out = subprocess.check_output(["git", "-C", path, "checkout", version.reference], universal_newlines=True)
+        print(out)
+        print("building")
+        print(path)
+        out = subprocess.check_output(["./build.sh", str(path)], universal_newlines=True)
+        print(str(out))
+    return path
+ 
 def main():
+    children = []
+    cores_max = cpu_count()
+    cores_used = 0
     while True:
-        #pull work package
-        print("pulling work package")
-        task, version, constraint, seed = get_work(cpu_count())
-        print("Work package " + str(task.path))
-        #clone git
-        path = PATH_GIT_REMOTE + version.reference + "/"
-        if not pathlib.Path(path).exists():
-            print("cloning into " + path)
-            out = subprocess.check_output(["git", "clone", REPO, path], universal_newlines=True)
-            print(out)
-            #pull from git
-            print("checkout " + version.reference)
-            out = subprocess.check_output(["git", "-C", path, "checkout", version.reference], universal_newlines=True)
-            print(out)
-            #build
-            print("building")
-            print(path)
-            out = subprocess.check_output(["./build.sh", str(path)], universal_newlines=True)
-            print(str(out))
-        #execute
-        print("executing " + path + "solver/build/solver")
-        print("Constraint: " + str(constraint.runtime_ms) + ", " + str(constraint.cores))
-        try:
-            out = execute.execute(path + "solver/build/solver", "../tasks/" + task.path, 1000 * 60 * 5, constraint.cores, seed)
-            print(str(out))
-            submit_work(task, version, constraint, seed, out)
-        except:
-            print("Exception occured, did not send result")
+        if cores_used + constraint.cores > cores_max:
+            #wait till something completes
+            sleep(1)
+        else:
+            print("pulling work package")
+            task, version, constraint, seed = get_work(cores_max)
+            print("Work package " + str(task.path))
+            path = clone_and_build(version)
+
+            print("executing " + path + "solver/build/solver")
+            print("Constraint: " + str(constraint.runtime_ms) + ", " + str(constraint.cores))
+            try:
+                out = execute.execute(path + "solver/build/solver", "../tasks/" + task.path, 1000 * 60 * 5, constraint.cores, seed)
+                print(str(out))
+                submit_work(task, version, constraint, seed, out)
+            except:
+                print("Exception occured, did not send result")
 
 if __name__ == "__main__":
     main()
