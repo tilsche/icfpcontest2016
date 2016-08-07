643018186367a355c856888c4b3e96233f6cb174
Mario Bielert <mario.bielert@googlemail.com>
2016-08-08 00:31:44 +0200

    NDEBUG

--- meta/build.sh
+++ meta/build.sh
@@ -2,4 +2,4 @@
 
 echo "Bash: $1/solver"
 
-cd "$1/solver" && mkdir -p build && cd build && cmake -DMIN_LOG_LEVEL=info -DCMAKE_BUILD_TYPE=Release .. && make solver
+cd "$1/solver" && mkdir -p build && cd build && cmake -DMIN_LOG_LEVEL=info -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=-O3 .. && make solver
