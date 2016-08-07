5584a1ddc8b64b906c162040d605c052d41a2541
Sven Fischer <sven.fischer85@gmx.de>
2016-08-07 16:11:15 +0200

    make meta build release

--- meta/build.sh
+++ meta/build.sh
@@ -2,5 +2,5 @@
 
 echo "Bash: $1/solver"
 
-cd "$1/solver" && mkdir -p build && cd build && cmake .. && make
+cd "$1/solver" && mkdir -p build && cd build && cmake -DMIN_LOG_LEVEL=info -DCMAKE_BUILD_TYPE=Release .. && make
 
