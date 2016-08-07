e603240342e2a1a6893f12016291ec143abf08b3
Sven Fischer <sven.fischer85@gmx.de>
2016-08-07 12:12:54 +0200

    fix get_tasks check wether file exists

--- meta/get_tasks.py
+++ meta/get_tasks.py
@@ -1,23 +1,34 @@
 import json
 import time
 import subprocess
-
+import os
 
 j = json.load(open("snapshot.1470560400.txt"))
 
 problems = j["problems"]
 
 def load(hash, id):
+    padded = '%05d' % id
+    print(padded)
+
+
+    mypath = "../tasks/"
+    myfile = mypath + padded + ".txt"
+    print(myfile)
+    if os.path.isfile(myfile):
+        print("file already exists")
+        return None 
     try:
         out = subprocess.check_output(["curl", "--compressed", "-L", "-H", "Expect:", "-H", 'X-API-Key: 191-d1229cf42703fee94dd0df16779b0074', 'http://2016sv.icfpcontest.org/api/blob/' + str(hash)], universal_newlines=True)
         print(out)
         if "Error" in out:
             return false
-        with open("tasks/" + str(id) + ".txt", "w+") as f:
+        with open(myfile, "w+") as f:
             f.write(out)
         return True
     except:
         return False
+
 for p in problems:
     hash = p["problem_spec_hash"]
     id = p["problem_id"]
@@ -25,7 +36,9 @@ for p in problems:
     correct = False
     while not correct:
         correct = load(hash, id)
-        if not correct:
+        if correct is None:
+            correct = True
+            continue
+        elif not correct:
             print("retry", hash, id)
-        time.sleep(1.5)
-
+        time.sleep(1.2)
