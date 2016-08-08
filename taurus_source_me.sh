0190f75e68ce48665a347ebb7539064c45ffe949
Thomas Ilsche <tilsche@users.noreply.github.com>
2016-08-08 15:05:51 +0200

    Delete taurus_source_me.sh

--- taurus_source_me.sh
+++
@@ -1,3 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/projects/zihforschung/cgal/lib/:/projects/zihforschung/mpfr/lib/
-export LIBRARY_PATH=$LIBRARY_PATH:/projects/zihforschung/cgal/lib/:/projects/zihforschung/mpfr/lib/
-export INCLUDE=$INCLUDE:/projects/zihforschung/cgal/include/:/projects/zihforschung/mpfr/include
